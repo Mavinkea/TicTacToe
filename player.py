eb00537ecab9146e7d2f5e0d9b36280efe0fd2ba
Aril <aril.mavinkere@stonybrook.edu>
2017-05-06 11:05:39 -0400

    replace changestate method, set default state to loggedin

--- player.py
+++ player.py
@@ -3,16 +3,19 @@ class Player:
 	def __init__(self, username, conn):
 		self.username=username
 		self.conn=conn
-		self.state="available"
-
-	def changeState(self):
-		if self.state=="available":
-			self.state="busy"
-		else:
-			self.state="available"
+		self.state="loggedin"
 
 	def send(self, msg):
 		self.conn.send(msg)
+
+	def setAvailable(self):
+		self.state="available"
+
+	def setBusy(self):
+		self.state="busy"
+
+	def setLoggedIn(self):
+		self.state="loggedin"
 
 
 

1f15913389774f216c7e959665973d67ca732560
Aril <aril.mavinkere@stonybrook.edu>
2017-05-07 00:03:01 -0400

    basic build

--- tttgame.py
+++ tttgame.py
@@ -1,7 +1,5 @@
 class TTTGame:
 
-	WIN_COMBINATIONS = [(0, 1, 2),(3, 4, 5),(6, 7, 8),(0, 3, 6),(1, 4, 7),(2, 5, 8),(0, 4, 8),(2, 4, 6),]
-
 	def __init__(self, player1, player2, gameID):
 		self.player1=player1
 		self.player2=player2
@@ -9,19 +7,21 @@ class TTTGame:
 		self.turn=player1
 		self.waiting=player2
 		self.board=['0','1','2','3','4','5','6','7','8']
+		self.WIN_COMBINATIONS = [(0, 1, 2),(3, 4, 5),(6, 7, 8),(0, 3, 6),(1, 4, 7),(2, 5, 8),(0, 4, 8),(2, 4, 6),]
 
 	def drawBoard(self):
-		return self.board[0]+" | "+self.board[1]+" | "+self.board[2]+"\n"+self.board[3]+" | "+self.board[4]+" | "+self.board[5]+"\n"+self.board[6]+" | "+self.board[7]+" | "+self.board[8]
+		return "\n"+self.board[0]+" | "+self.board[1]+" | "+self.board[2]+"\n"+self.board[3]+" | "+self.board[4]+" | "+self.board[5]+"\n"+self.board[6]+" | "+self.board[7]+" | "+self.board[8]
 
 	def changeTurn(self):
-		if self.turn==self.player1:
+		if self.turn==self.player2:
+			self.turn=self.player1
+			self.waiting=self.player2
+		else:
 			self.turn=self.player2
 			self.waiting=self.player1
-		else:
-			self.turn=self.player1
-			self.waiting=player2
 
 	def makeMove(self, move):
+		move=int(move)
 		if self.turn==self.player1:
 			self.board[move]='X'
 		else:
@@ -29,12 +29,12 @@ class TTTGame:
 
 		done=True
 		#Check if there are still moves to be made
-		for i in board:
+		for i in self.board:
 			if i!='X' or i!='O':
 				done=False
 
 		#Check for winning combinations
-		for a,b,c in WIN_COMBINATIONS:
+		for a,b,c in self.WIN_COMBINATIONS:
 			if self.board[a]==self.board[b]==self.board[c]:
 				done=True
 
@@ -43,7 +43,6 @@ class TTTGame:
 			return "300 FIN"
 		#If not endgame, change turn
 		else:
-			self.changeTurn()
 			return "301 NPT"
 
 			
